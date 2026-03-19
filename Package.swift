662dc89699064614617dd26e47ee6d054ddca2c9
Aurimas Petrevicius <aurimas@notnot.xyz>
2026-03-19 10:02:28 +0200

    update functionality and initial theme

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "espnpredictor", targets: ["espnpredictor"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Engagecraft-Solutions/gaming-core-ios-espn", exact: "1.0.2"),
+        .package(url: "https://github.com/Engagecraft-Solutions/gaming-core-ios-espn", exact: "1.0.3"),
         .package(url: "https://github.com/Alamofire/Alamofire", exact: "5.9.1"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", exact: "5.0.2"),
         .package(url: "https://github.com/onevcat/Kingfisher", from: "7.11.0"),
@@ -21,6 +21,7 @@ let package = Package(
             dependencies: [
                 "bpredictor",
                 .product(name: "GamesLib", package: "gaming-core-ios-espn"),
+                .product(name: "GHMainTheme", package: "gaming-core-ios-espn"),
                 .product(name: "Alamofire", package: "Alamofire"),
                 .product(name: "SwiftyJSON", package: "SwiftyJSON"),
                 .product(name: "Kingfisher", package: "Kingfisher"),
